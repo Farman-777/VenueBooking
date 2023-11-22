73be08466a74a21610304de9cad3914ad912bdc2
Farman-777 <farmanmalik4487@gmail.com>
2023-11-22 07:45:59 +0530

    remove button updated

--- booking/src/Component/Venue/BookVenue.js
+++ booking/src/Component/Venue/BookVenue.js
@@ -1,11 +1,111 @@
+// import React, { useEffect, useState } from "react";
+// import axios from "axios";
+// import Swal from "sweetalert2";
+
+// const BookVenue = ({ handleClose, CartId , id , }) => {
+//   console.log("ID in bookVenue using Props : ", id);
+//   const [bookingDate, setBookingDate] = useState("");
+  
+//   const getCurrentDate = () => {
+//     const today = new Date();
+//     const year = today.getFullYear();
+//     const month = (today.getMonth() + 1).toString().padStart(2, "0");
+//     const day = today.getDate().toString().padStart(2, "0");
+//     return `${year}-${month}-${day}`;
+//   };
+  
+
+//   const handleBookingDateChange = (e) => { setBookingDate(e.target.value); };
+
+//   const handleSubmit = async (e) => { e.preventDefault(); 
+
+//     // Parse the bookingDate string into a Date object
+//     const dateParts = bookingDate.split("-");
+//     const formattedDate = new Date( dateParts[0], dateParts[1] - 1, dateParts[2] );
+
+//     const day = formattedDate.getDate().toString().padStart(2, "0");
+//     const month = (formattedDate.getMonth() + 1).toString().padStart(2, "0");
+//     const year = formattedDate.getFullYear().toString().slice(-2);
+
+//     const formattedDateStr = `${day}-${month}-${year}`;
+
+//     const bookingData = { Id: CartId, Date: formattedDateStr, Status: "Booked", };
+
+//    // Sending a POST request to create a booking record
+// axios.post("http://localhost:8000/venueRecords", bookingData)
+// .then((response) => {
+//   if (response.status === 200) {
+
+//     axios.put(`http://localhost:8006/updateBookCountCart/${id}`, { UpdateBookCount: 1, })
+//     .then((response) => {  Swal.fire("Booking Status","Your booking has been successful!","success",); })
+//     .catch((error) => { Swal.fire("Error", "Failed to update VenueBook status", "error"); });
+
+//     setBookingDate("");
+//   } else { console.error("Unexpected response status:", response.status); }
+// }).catch((error) => {Swal.fire("Already Booked", "Please Select Different Date", "error"); });
+
+
+//   axios.get(`http://localhost:8000/getVenueRecord?id=${CartId}`)
+//   .then((response) => { console.log(response); })
+//   .catch((error) => {   console.error("Error while retrieving venue records:", error); });
+// };
+
+//   return (
+//     <div className="container">
+//       <div className="row justify-content-center">
+//         <div className="col-md-12">
+//           <div className="card">
+//             <div
+//               className="card-header p-0"
+//               style={{ backgroundColor: "#007bff", color: "#fff", textAlign: "center" }}
+//             >
+//               <h1>Book a Venue</h1>
+//             </div>
+//             <div className="card-body">
+//               <form onSubmit={handleSubmit}>
+//                 <div className="mb-3">
+//                   <label htmlFor="bookingDate" className="form-label" style={{ fontWeight: "bold" }}>
+//                     Booking Date:
+//                   </label>
+//                   <input
+//                     type="date"
+//                     className="form-control"
+//                     id="bookingDate"
+//                     value={bookingDate}
+//                     onChange={handleBookingDateChange}
+//                     required
+//                     min={getCurrentDate()}
+//                   />
+//                 </div>
+//                 <button type="submit" className="btn btn-primary" style={{ backgroundColor: "#007bff", border: "none" }}>
+//                   Book Venue
+//                 </button>
+//               </form>
+//             </div>
+//           </div>
+//           <div className="text-end mt-4">
+//             <button type="button" className="btn btn-danger" onClick={handleClose}>
+//               Close
+//             </button>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default BookVenue;
+
+
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import Swal from "sweetalert2";
 
+
 const BookVenue = ({ handleClose, CartId , id , }) => {
   console.log("ID in bookVenue using Props : ", id);
   const [bookingDate, setBookingDate] = useState("");
-  
+ 
   const getCurrentDate = () => {
     const today = new Date();
     const year = today.getFullYear();
@@ -13,42 +113,54 @@ const BookVenue = ({ handleClose, CartId , id , }) => {
     const day = today.getDate().toString().padStart(2, "0");
     return `${year}-${month}-${day}`;
   };
-  
+ 
+
 
   const handleBookingDateChange = (e) => { setBookingDate(e.target.value); };
 
-  const handleSubmit = async (e) => { e.preventDefault(); 
+
+  const handleSubmit = async (e) => { e.preventDefault();
+
 
     // Parse the bookingDate string into a Date object
     const dateParts = bookingDate.split("-");
     const formattedDate = new Date( dateParts[0], dateParts[1] - 1, dateParts[2] );
 
+
     const day = formattedDate.getDate().toString().padStart(2, "0");
     const month = (formattedDate.getMonth() + 1).toString().padStart(2, "0");
     const year = formattedDate.getFullYear().toString().slice(-2);
 
-    const formattedDateStr = `${day}-${month}-${year}`;
+
+    const formattedDateStr = `${year}-${month}-${day}`;
+
 
     const bookingData = { Id: CartId, Date: formattedDateStr, Status: "Booked", };
+
 
    // Sending a POST request to create a booking record
 axios.post("http://localhost:8000/venueRecords", bookingData)
 .then((response) => {
   if (response.status === 200) {
 
+
     axios.put(`http://localhost:8006/updateBookCountCart/${id}`, { UpdateBookCount: 1, })
     .then((response) => {  Swal.fire("Booking Status","Your booking has been successful!","success",); })
     .catch((error) => { Swal.fire("Error", "Failed to update VenueBook status", "error"); });
+
 
     setBookingDate("");
   } else { console.error("Unexpected response status:", response.status); }
 }).catch((error) => {Swal.fire("Already Booked", "Please Select Different Date", "error"); });
 
 
+
+
   axios.get(`http://localhost:8000/getVenueRecord?id=${CartId}`)
   .then((response) => { console.log(response); })
   .catch((error) => {   console.error("Error while retrieving venue records:", error); });
 };
+
 
   return (
     <div className="container">
@@ -94,4 +206,7 @@ axios.post("http://localhost:8000/venueRecords", bookingData)
   );
 };
 
+
 export default BookVenue;
+
+
--- booking/src/Navbar/CartItem.js
+++ booking/src/Navbar/CartItem.js
@@ -3,21 +3,26 @@ import React, { useEffect, useState } from "react";
 import Swal from "sweetalert2";
 import axios from "axios";
 
+
 import ModalComp from "../ModalComp";
+
 
 import BookPhotographer from '../Component/PhotoGrapher/BookPhotographer'
 import BookCater from "../Component/Cater/BookCater";
 import BookDJ from "../Component/DJ/BookDJ";
 import BookVenue from "../Component/Venue/BookVenue";
 
+
 const CartItem = ({ name, price, imageUrl, imageName, id, getData ,keyName,CartId ,BookCount}) => {
   const {isAuthenticatedUser} = useSelector(state => state.root)
   const [showModal,setShowModal] = useState(false);
-  
-  console.log("id : ", id); 
+  const [selectedDate, setSelectedDate] = useState("");
+ 
+  console.log("id : ", id);
   console.log("name : ", name);
   console.log(`${imageUrl}${imageName}`);
   console.log("keyName : ",keyName);
+
 
   const handleClick = () => {
     if (isAuthenticatedUser) {
@@ -27,43 +32,255 @@ const CartItem = ({ name, price, imageUrl, imageName, id, getData ,keyName,CartI
     }
   };
 
+
   const obj = { CartKey1: id };
   console.log(obj.CartKey1);
 
-  const handleRemove = () => {   
-    axios.put(`http://localhost:8006/removeBookCountCart/${id}`, { UpdateBookCount: 1, })
-    .then((response) => { Swal.fire("Updated!", "Venue Book status has been updated.", "success"); })
-    .catch((error) => {     
-      axios.post("http://localhost:8006/deleteCart", obj)
-      .then((response) => {
-      if (response.status === 200) { Swal.fire({ icon: "success", title: "Cart Item Deleted", text: "Successfully!",  }); 
-      
-    }
-    });
-  });
+
+  const deleteVenueBookDateRecord = () => {
+    if(selectedDate !== "" && selectedDate !== undefined){
+    const year = selectedDate.split("-")[0].slice(-2);
+    const formattedDate = `${year}-${selectedDate.split("-")[1]}-${selectedDate.split("-")[2]}`;
+    console.log(formattedDate,CartId)
+    axios.post("http://localhost:8000/removeVenueBookDate",{Id:CartId,Date:formattedDate})
+    .then(result => {
+      if(result.status === 200) {
+        Swal.fire("Success!","Selected date Record Deleted Successfully","success")
+        handleRemove();
+      }
+    })
+    .catch((error) => {
+      if(error.response && error.response.status === 404)
+      Swal.fire("Error!","Record not exist","error");
+    })
+    } else { return }
+  }
+
+
  
-}
+ 
+  const deleteCaterBookDateRecord = () => {
+    if(selectedDate !== "" && selectedDate !== undefined){
+    const year = selectedDate.split("-")[0].slice(-2);
+    const formattedDate = `${year}-${selectedDate.split("-")[1]}-${selectedDate.split("-")[2]}`;
+    console.log(formattedDate,CartId)
+    axios.post("http://localhost:8002/removeCaterBookDate",{Id:CartId,Date:formattedDate})
+    .then(result => {
+      if(result.status === 200) {
+        Swal.fire("Success!","Selected date Record Deleted Successfully","success")
+        handleRemove();
+      }
+    })
+    .catch((error) => {
+      if(error.response && error.response.status === 404)
+      Swal.fire("Error!","Record not exist","error");
+    })
+    } else { return }
+  }
 
 
-  return (
-    <>
-      <div className="cart-parent">
-        <img src={`${imageUrl}${imageName}`} alt="" />
-        <div className="item-details"><div className="item-info"> <span>{name}</span> <p>Price: {price}</p> </div> <span>Booked {BookCount} Times </span> </div>
-        <button type="date" className="btn btn-success" onClick={() => {handleClick();}}>Select Date</button>
-        <button className="btn btn-danger" onClick={() => handleRemove()}>Remove</button>
-      </div>
-      <ModalComp show={showModal} width={"90%"}
-        modalBody={
-        (keyName === "DJName") ? <BookDJ handleClose={() => setShowModal(false)} CartId={CartId} id={id}/> :
-        (keyName === "CaterName") ? <BookCater handleClose={() => setShowModal(false)} CartId={CartId} id={id}/> :
-        (keyName === "PhotoGrapherName") ? <BookPhotographer handleClose={() => setShowModal(false)} CartId={CartId} id={id}/> :
-        (keyName === "VenueName") ? <BookVenue handleClose={() => setShowModal(false)} CartId={CartId} id={id}/> :
-        null
+
+
+  const deleteDJBookDateRecord = () => {
+    if(selectedDate !== "" && selectedDate !== undefined){
+    const year = selectedDate.split("-")[0].slice(-2);
+    const formattedDate = `${year}-${selectedDate.split("-")[1]}-${selectedDate.split("-")[2]}`;
+    console.log(formattedDate,CartId)
+    axios.post("http://localhost:8003/removeDJBookDate",{Id:CartId,Date:formattedDate})
+    .then(result => {
+      if(result.status === 200) {
+        Swal.fire("Success!","Selected date Record Deleted Successfully","success")
+        handleRemove();
+      }
+    })
+    .catch((error) => {
+      if(error.response && error.response.status === 404)
+      Swal.fire("Error!","Record not exist","error");
+    })
+    } else { return }
+  }
+ 
+  const deletePhotoGrapherBookDateRecord = () => {
+    if(selectedDate !== "" && selectedDate !== undefined){
+    const year = selectedDate.split("-")[0].slice(-2);
+    const formattedDate = `${year}-${selectedDate.split("-")[1]}-${selectedDate.split("-")[2]}`;
+    console.log(formattedDate,CartId)
+    axios.post("http://localhost:8001/removePhotoGrapherBookDate",{Id:CartId,Date:formattedDate})
+    .then(result => {
+      if(result.status === 200) {
+        Swal.fire("Success!","Selected date Record Deleted Successfully","success")
+        handleRemove();
+      }
+    })
+    .catch((error) => {
+      if(error.response && error.response.status === 404)
+      Swal.fire("Error!","Record not exist","error");
+    })
+    } else { return }
+  }
+
+
+  const handleRemove = async () => {
+    await axios.put(`http://localhost:8006/removeBookCountCart/${id}`, { UpdateBookCount: 1 });
+  }
+ 
+const handleDelete = () => {
+  axios.post("http://localhost:8006/deleteCart", obj)
+  .then((response) => {
+    if (response.status === 200) {
+      Swal.fire({
+        icon: "success",
+        title: "Cart Item Deleted",
+        text: "Cart item has been deleted successfully!",
+      });
+      if(keyName === "VenueName") {axios.post("http://localhost:8000/deleteVenueDateRecord", { Id:CartId }); getData();}
+      else if( keyName === "CaterName"){axios.post("http://localhost:8002/deleteCaterDateRecord", { Id:CartId }); getData();}
+      else if( keyName === "DJName"){axios.post("http://localhost:8003/deleteDJDateRecord", { Id:CartId }); getData();}
+      else if( keyName === "PhotoGrapherName"){axios.post("http://localhost:8001/deletePhotoGrapherDateRecord", { Id:CartId }); getData();}
     }
-      />
-    </>
-  );
+  });
+}
+ 
+
+
+
+
+return (
+  <>
+    <div className="cart-parent">
+      <img src={`${imageUrl}${imageName}`} alt="" />
+      <div className="item-details">
+        <div className="item-info">
+          <span>{name}</span>
+          <p>Price: {price}</p>
+        </div>
+        <span>Booked {BookCount} Times </span>
+      </div>
+      <button type="date" className="btn btn-success" onClick={() => handleClick()}>Select Date</button>
+      <div className="d-flex">
+        {(() => {
+          switch (keyName) {
+            case "VenueName":
+              return (
+                <div>
+                  <input type="date" value={selectedDate} onChange={(e) => setSelectedDate(e.target.value)} />
+                  <button className="btn btn-primary" onClick={deleteVenueBookDateRecord}>Remove</button>
+                </div>
+              );
+            case "CaterName":
+              return (
+                <div>
+                  <input type="date" value={selectedDate} onChange={(e) => setSelectedDate(e.target.value)} />
+                  <button className="btn btn-primary" onClick={deleteCaterBookDateRecord}>Remove</button>
+                </div>
+              );
+              case "DJName":
+                return (
+                  <div>
+                    <input type="date" value={selectedDate} onChange={(e) => setSelectedDate(e.target.value)} />
+                    <button className="btn btn-primary" onClick={deleteDJBookDateRecord}>Remove</button>
+                  </div>
+                );
+                case "PhotoGrapherName":
+                  return (
+                    <div>
+                      <input type="date" value={selectedDate} onChange={(e) => setSelectedDate(e.target.value)} />
+                      <button className="btn btn-primary" onClick={deletePhotoGrapherBookDateRecord}>Remove</button>
+                    </div>
+                  );
+            default:
+              return null;
+          }
+        })()}
+      </div>
+      <button className="btn btn-danger" onClick={() => handleDelete()}>Delete</button>
+    </div>
+    <ModalComp
+      show={showModal}
+      width={"90%"}
+      modalBody={
+        (keyName === "DJName") ? <BookDJ handleClose={() => setShowModal(false)} CartId={CartId} id={id} /> :
+        (keyName === "CaterName") ? <BookCater handleClose={() => setShowModal(false)} CartId={CartId} id={id} /> :
+        (keyName === "PhotoGrapherName") ? <BookPhotographer handleClose={() => setShowModal(false)} CartId={CartId} id={id} /> :
+        (keyName === "VenueName") ? <BookVenue handleClose={() => setShowModal(false)} CartId={CartId} id={id} /> :
+        null
+      }
+    />
+  </>
+);
+
+
 };
 
+
 export default CartItem;
+
+// import { useDispatch, useSelector } from "react-redux";
+// import React, { useEffect, useState } from "react";
+// import Swal from "sweetalert2";
+// import axios from "axios";
+
+// import ModalComp from "../ModalComp";
+
+// import BookPhotographer from '../Component/PhotoGrapher/BookPhotographer'
+// import BookCater from "../Component/Cater/BookCater";
+// import BookDJ from "../Component/DJ/BookDJ";
+// import BookVenue from "../Component/Venue/BookVenue";
+
+// const CartItem = ({ name, price, imageUrl, imageName, id, getData ,keyName,CartId ,BookCount}) => {
+//   const {isAuthenticatedUser} = useSelector(state => state.root)
+//   const [showModal,setShowModal] = useState(false);
+  
+//   console.log("id : ", id); 
+//   console.log("name : ", name);
+//   console.log(`${imageUrl}${imageName}`);
+//   console.log("keyName : ",keyName);
+
+//   const handleClick = () => {
+//     if (isAuthenticatedUser) {
+//       setShowModal(true);
+//     } else {
+//       Swal.fire('error','User Not Logged In','Please log in to continue.',);
+//     }
+//   };
+
+//   const obj = { CartKey1: id };
+//   console.log(obj.CartKey1);
+
+//   const handleRemove = () => {   
+//     axios.put(`http://localhost:8006/removeBookCountCart/${id}`, { UpdateBookCount: 1, })
+//     .then((response) => { Swal.fire("Updated!", "Venue Book status has been updated.", "success"); })
+//     .catch((error) => {     
+//       axios.post("http://localhost:8006/deleteCart", obj)
+//       .then((response) => {
+//       if (response.status === 200) { Swal.fire({ icon: "success", title: "Cart Item Deleted", text: "Successfully!",  }); 
+      
+//     }
+//     });
+//   });
+ 
+// }
+
+
+//   return (
+//     <>
+//       <div className="cart-parent">
+//         <img src={`${imageUrl}${imageName}`} alt="" />
+//         <div className="item-details"><div className="item-info"> <span>{name}</span> <p>Price: {price}</p> </div> <span>Booked {BookCount} Times </span> </div>
+//         <button type="date" className="btn btn-success" onClick={() => {handleClick();}}>Select Date</button>
+//         <button className="btn btn-danger" onClick={() => handleRemove()}>Remove</button>
+//       </div>
+//       <ModalComp show={showModal} width={"90%"}
+//         modalBody={
+//         (keyName === "DJName") ? <BookDJ handleClose={() => setShowModal(false)} CartId={CartId} id={id}/> :
+//         (keyName === "CaterName") ? <BookCater handleClose={() => setShowModal(false)} CartId={CartId} id={id}/> :
+//         (keyName === "PhotoGrapherName") ? <BookPhotographer handleClose={() => setShowModal(false)} CartId={CartId} id={id}/> :
+//         (keyName === "VenueName") ? <BookVenue handleClose={() => setShowModal(false)} CartId={CartId} id={id}/> :
+//         null
+//     }
+//       />
+//     </>
+//   );
+// };
+
+// export default CartItem;
